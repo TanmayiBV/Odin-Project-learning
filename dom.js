3edf11f362b463c6ab050183f64de2c3bac0718d
Tanmayi B V <tanmayibv2006@gmail.com>
2025-08-21 14:13:08 +0530

    Add div

--- dom.js
+++ dom.js
@@ -3,3 +3,16 @@ const elem1 = document.createElement("p")
 elem1.style.color = "red"
 elem1.innerText = "Hey, I m red"
 parent.appendChild(elem1)
+const elem2 = document.createElement("div")
+elem2.classList.add("parent2")
+elem2.style.border = "black"
+elem2.style.backgroundColor = "pink"
+parent.appendChild(elem2)
+
+const div = document.querySelector(".parent2")
+const h1 = document.createElement("h1")
+h1.innerText="I m in div"
+const p = document.createElement("p")
+p.innerText="ME TOO"
+div.appendChild(h1)
+div.appendChild(p)
