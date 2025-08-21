666444d570d73e519b2b7023eb030f9e9db3f249
Tanmayi B V <tanmayibv2006@gmail.com>
2025-08-21 13:58:22 +0530

    Add red paragraph

---
+++ dom.js
@@ -0,0 +1,5 @@
+const parent = document.querySelector("#container")
+const elem1 = document.createElement("p")
+elem1.style.color = "red"
+elem1.innerText = "Hey, I m red"
+parent.appendChild(elem1)
