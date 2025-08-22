6a950430f505247bf8f24db7a7b19f7d64dc70e4
Tanmayi B V <tanmayibv2006@gmail.com>
2025-08-22 17:54:10 +0530

    Boxes remain the same color once gotten with increase in opacity for each hover

--- et-a-schetch.js
+++ et-a-schetch.js
@@ -14,13 +14,17 @@ function createGrid(squares = 16) {
         for (let j = 0; j < squares; j++) {
             let box = document.createElement("div")
             box.classList.add("box")
-            box.dataset.opacity = 0;
+
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            let opacity = 0;
+            box.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+
             box.addEventListener("mouseenter", () => {
-                let currentOpacity = parseFloat(box.dataset.opacity);
-                currentOpacity = Math.min(currentOpacity + 0.3, 1);
-                box.dataset.opacity = currentOpacity;
-                box.style.backgroundColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${currentOpacity})`
-            })
+                opacity = Math.min(opacity + 0.2, 1);
+                box.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+            });
             row.appendChild(box)
         }
         container.append(row)
